128c09e54ad611f8cae4300dd75ef8f20fd6b8a4
Ioana Stroe <ioana.stroe@fortech.ro>
2024-11-19 16:23:50 +0200

    Explored jetpack box component

--- app/src/main/java/com/example/explorecompose/MainActivity.kt
+++ app/src/main/java/com/example/explorecompose/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
@@ -14,6 +16,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -24,6 +28,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.explorecompose.ui.theme.ExploreComposeTheme
 import com.example.explorecompose.ui.theme.Typography
 
@@ -33,91 +38,42 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ExploreComposeTheme {
-//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//
-//                }
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting()
+                }
             }
         }
     }
 }
 
 @Composable
-fun ColumnScope.CustomItem(width : Int, height : Int, weight : Float, color : Color){
-    Surface(
+fun Greeting(){
+    Box(
+        contentAlignment = Alignment.BottomEnd,
         modifier = Modifier
-            .padding(16.dp)
-            .width(width.dp)
-            .height(height.dp),
-        color = color
-    ) {}
+            .fillMaxSize()
+            .padding(20.dp)){
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+            .background(color = Color.Cyan)){
+            Box(modifier = Modifier.
+                height(100.dp)
+                .width(100.dp)
+                .background(color = Color.Magenta))
+            Text(
+                text = "Hello, I love Android",
+                style = Typography.headlineSmall,
+                fontSize = 30.sp
+                )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ExploreComposeTheme {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.End,
-            verticalArrangement = Arrangement.Top
-        ) {
-            CustomItem(width = 200, height = 100, weight = 3f, color = MaterialTheme.colorScheme.primary)
-            CustomItem(width = 100, height = 100, weight = 3f, color = MaterialTheme.colorScheme.secondary)
-            CustomItem(width = 50, height = 100, weight = 3f, color = MaterialTheme.colorScheme.tertiary)
-        }
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.Start
-        ) {
-            Surface(
-                modifier = Modifier
-                    .padding(16.dp)
-                    .weight(1f)
-                    .width(150.dp),
-                color = MaterialTheme.colorScheme.primaryContainer
-            ) {}
-            Surface(
-                modifier = Modifier
-                    .padding(16.dp)
-                    .weight(3f)
-                    .width(150.dp),
-                color = MaterialTheme.colorScheme.secondaryContainer
-            ) {}
-            Surface(
-                modifier = Modifier
-                    .padding(16.dp)
-                    .weight(2f)
-                    .width(150.dp),
-                color = MaterialTheme.colorScheme.tertiaryContainer
-            ) {}
-        }
-        Row(
-            modifier = Modifier.fillMaxSize(),
-            verticalAlignment = Alignment.Bottom,
-            horizontalArrangement = Arrangement.End
-        )
-        {
-            Surface(
-                modifier = Modifier
-                    .padding(8.dp)
-                    .height(350.dp)
-                    .width(20.dp),
-                color = MaterialTheme.colorScheme.inversePrimary
-            ) {}
-            Surface(
-                modifier = Modifier
-                    .padding(8.dp)
-                    .height(350.dp)
-                    .width(30.dp),
-                color = MaterialTheme.colorScheme.errorContainer
-            ) {}
-            Surface(
-                modifier = Modifier
-                    .padding(8.dp)
-                    .height(350.dp)
-                    .width(100.dp),
-                color = MaterialTheme.colorScheme.error
-            ) {}
-        }
+        Greeting()
     }
 }
